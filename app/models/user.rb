988c96c0b6f0118847da65f2a0337ca41981c433
Lex Alexander <l.alexander10@gmail.com>
2014-09-04 18:20:53 -0700

    made a fundamental path error :( while trying to create logout link. I fixed it now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true 
 	validates :password_confirmation, presence: true
+	
 end
